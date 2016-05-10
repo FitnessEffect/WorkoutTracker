b856ecb4d0fda148c2bb11b3bcdc4bd79d2e18b6
FitnessEffect <stefan.auvergne@gmail.com>
2016-05-10 17:50:21 -0400

    delete

--- OneRMViewController.swift
+++ OneRMViewController.swift
@@ -53,14 +53,4 @@ class OneRMViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         
         dismissViewControllerAnimated(true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
