5a2a5f329cd1a44aac1a4ca3c17353cda6b8b501
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-05-27 22:35:52 -0400

    edit exercise fix

---
+++ WorkoutTracker/ChallengeCustomCell.swift
@@ -0,0 +1,28 @@
+//
+//  ChallengesTableViewCell.swift
+//  WorkoutTracker
+//
+//  Created by Stefan Auvergne on 5/27/17.
+//  Copyright © 2017 Stefan Auvergne. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeCustomCell: UITableViewCell {
+
+    @IBOutlet weak var titleOutlet: UILabel!
+    @IBOutlet weak var numberOutlet: UILabel!
+    @IBOutlet weak var challenger: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
