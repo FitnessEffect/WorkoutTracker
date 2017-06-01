480d97fe7ec10728d5f4d57975bba5259bbc0292
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-06-01 11:14:57 -0400

    picker views fix

--- ArmViewController.swift
+++ ArmViewController.swift
@@ -13,6 +13,7 @@ class ArmViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     
     @IBOutlet weak var pickerOutlet: UIPickerView!
     @IBOutlet weak var add: UIButton!
+    @IBOutlet weak var repsSetsOutlet: UIPickerView!
     
     let exerciseKey:String = "exerciseKey"
     var myExercise = Exercise()
@@ -69,8 +70,11 @@ class ArmViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     
     @IBAction func addExercise(_ sender: UIButton) {
         let id:Int = pickerOutlet.selectedRow(inComponent: 0)
+        let idReps = repsSetsOutlet.selectedRow(inComponent: 1)
+        let idSets = repsSetsOutlet.selectedRow(inComponent: 0)
+        
         myExercise.name = "Arms"
-        myExercise.exerciseDescription = armExercises[id] + "|" + reps[id] + " - " + sets[id]
+        myExercise.exerciseDescription = armExercises[id] + "|" + reps[idReps] + " - " + sets[idSets]
         myExercise.category = "Arms"
     
         NotificationCenter.default.post(name: Notification.Name(rawValue: "getExerciseID"), object: nil, userInfo: [exerciseKey:myExercise])
@@ -84,9 +88,9 @@ class ArmViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
         if pickerView.tag == 0 {
             label.text = armExercises[row]
         }else if component == 0{
-            label.text = sets[row]
-        }else{
             label.text = reps[row]
+        }else{
+            label.text = sets[row]
         }
         let myTitle = NSAttributedString(string: label.text!, attributes: [NSFontAttributeName:UIFont(name: "Have a Great Day Demo", size: 25.0)!,NSForegroundColorAttributeName:UIColor.black])
         label.attributedText = myTitle
