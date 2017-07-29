e48055b32a774b1f197f65f5f1a9342f132ba7f4
Stefan Auvergne <stefanauvergne@Stefans-MacBook-Pro.local>
2017-07-28 23:19:05 -0400

    superset cell textfield replaced textView

--- SupersetViewController.swift
+++ SupersetViewController.swift
@@ -64,7 +64,7 @@ class SupersetViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "supersetCell")! as! SupersetTableViewCell
-        cell.descriptionTextView.text = self.exercises[indexPath.row].exerciseDescription
+        cell.descriptionTextField.text = self.exercises[indexPath.row].exerciseDescription
         cell.numLabel.text = String(indexPath.row + 1)
         cell.backgroundColor = UIColor.clear
         cell.tag = indexPath.row
