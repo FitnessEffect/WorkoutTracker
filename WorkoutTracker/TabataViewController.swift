21638ff45f6c575ee556f5357e13c710aa280a91
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-07-29 01:13:32 -0400

    title renamed + resizing of pickerView elements

--- WorkoutTracker/TabataViewController.swift
+++ WorkoutTracker/TabataViewController.swift
@@ -165,7 +165,7 @@ class TabataViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         }else{
             label.text = totalTime[row]
         }
-        let myTitle = NSAttributedString(string: label.text!, attributes: [NSFontAttributeName:UIFont(name: "Have a Great Day", size: 23.0)!,NSForegroundColorAttributeName:UIColor.black])
+        let myTitle = NSAttributedString(string: label.text!, attributes: [NSFontAttributeName:UIFont(name: "Have a Great Day", size: 21.0)!,NSForegroundColorAttributeName:UIColor.black])
         label.attributedText = myTitle
         label.textAlignment = NSTextAlignment.center
         return label
