aa98dc102a85090d89bc0a01cba1869291844d20
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-10-28 11:07:15 -0400

    DeleteProgressDataViewController Added

---
+++ WorkoutTracker/DeleteProgressDataViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DeleteProgressDataViewController.swift
+//  WorkoutTracker
+//
+//  Created by Stefan Auvergne on 10/28/17.
+//  Copyright © 2017 Stefan Auvergne. All rights reserved.
+//
+
+import UIKit
+
+class DeleteProgressDataViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var dataValues = [(key: String, value: String)]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataValues.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = self.dataValues[indexPath.row].key
+        cell.backgroundColor = UIColor.clear
+        cell.tag = indexPath.row
+        cell.accessibilityIdentifier = "Type" + String(indexPath.row)
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
